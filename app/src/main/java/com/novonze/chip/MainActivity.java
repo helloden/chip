b48b674b69cffd86367490c607880536ea73c31f
Denise <denisekwokk@gmail.com>
2018-03-02 00:11:20 -0800

    Fetches the foreign exchange rate from fixer.io even though it's a little hacky in caching the data...

--- app/src/main/java/com/novonze/chip/MainActivity.java
+++ app/src/main/java/com/novonze/chip/MainActivity.java
@@ -1,8 +1,10 @@
 package com.novonze.chip;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -10,21 +12,31 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.DecimalFormat;
+import java.util.HashMap;
 
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    SquareButton    button1, button2, button3,
-                    button4, button5, button6,
-                    button7, button8, button9,
-                    button0, buttonDot, buttonDel;
+    SquareButton button1, button2, button3,
+            button4, button5, button6,
+            button7, button8, button9,
+            button0, buttonDot, buttonDel;
 
     TextView inputText, resultText, inputCurrencyLabel, resultCurrencyLabel;
 
     DecimalFormat decimalFormat;
 
     CheckBox checkBox;
+
+    Double UsdToCadBaseRate = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         checkBox.setOnCheckedChangeListener(
                 new CompoundButton.OnCheckedChangeListener() {
                     @Override
-                    public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                         recalculateFinalValue();
                     }
                 }
@@ -50,13 +62,13 @@ public class MainActivity extends AppCompatActivity {
 
         inputCurrencyLabel = (TextView) findViewById(R.id.inputCurrencyLabel);
         resultCurrencyLabel = (TextView) findViewById(R.id.resultCurrencyLabel);
+        new FetchExchangeRates().execute("USD");
     }
 
-    public void onClickCalculatorButton(View v)
-    {
+    public void onClickCalculatorButton(View v) {
         String currentText = inputText.getText().toString();
-        Button b = (SquareButton)v;
-        switch(v.getId()){
+        Button b = (SquareButton) v;
+        switch (v.getId()) {
             case R.id.buttonDel:
                 handleBackspace(currentText);
                 break;
@@ -71,19 +83,18 @@ public class MainActivity extends AppCompatActivity {
         recalculateFinalValue();
     }
 
-    public void onClickSwapButton(View v)
-    {
+    public void onClickSwapButton(View v) {
         CharSequence oldInputCurrency = inputCurrencyLabel.getText();
         inputCurrencyLabel.setText(resultCurrencyLabel.getText());
         resultCurrencyLabel.setText(oldInputCurrency);
         recalculateFinalValue();
+
     }
 
     private void handleDot(String currentText) {
-        if(!currentText.contains(".")){
+        if (!currentText.contains(".")) {
             inputText.setText(inputText.getText() + ".");
-        }
-        else {
+        } else {
             Context context = getApplicationContext();
             CharSequence text = "Only one decimal point duh dummy.";
             int duration = Toast.LENGTH_SHORT;
@@ -93,45 +104,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void handleBackspace(String currentText) {
-        if(currentText.length() == 1) {
+        if (currentText.length() == 1) {
             inputText.setText("0");
-        }
-        else {
+        } else {
             inputText.setText(currentText.substring(0, currentText.length() - 1));
         }
     }
 
     private void updateInputValue(String value) {
         String currentText = inputText.getText().toString();
-        if(currentText.equals("0")){
+        if (currentText.equals("0")) {
             inputText.setText(value);
-        }
-        else{
+        } else {
             inputText.setText(inputText.getText() + value);
         }
     }
 
-    private void recalculateFinalValue()
-    {
+    private void recalculateFinalValue() {
         double currentInputDouble = Double.parseDouble(inputText.getText().toString());
         String originatingCurrency = inputCurrencyLabel.getText().toString();
         double finalValue = currentInputDouble * getCurrencyExchangeRate(originatingCurrency);
 
-        if(checkBox.isChecked())
-        {
+        if (checkBox.isChecked()) {
             finalValue *= getOriginatingTaxRate(originatingCurrency);
         }
 
         resultText.setText(decimalFormat.format(finalValue));
     }
 
-    private double getCurrencyExchangeRate(String originating)
-    {
-        switch(originating) {
+    private double getCurrencyExchangeRate(String baseCurrency) {
+        switch (baseCurrency) {
             case "USD":
-                return 1/0.74;
+                return UsdToCadBaseRate;
             case "CAD":
-                return 0.74;
+                return 1/UsdToCadBaseRate;
             default:
                 return 1;
         }
@@ -145,6 +151,65 @@ public class MainActivity extends AppCompatActivity {
                 return 1.12;
             default:
                 return 1;
+        }
+    }
+
+    private class FetchExchangeRates extends AsyncTask<String, Integer, HashMap<String, Double>> {
+
+        @Override
+        protected HashMap<String, Double> doInBackground(String... params) {
+            try {
+                String baseCurrency = (String) params[0];
+                URL myUrl = new URL("https://api.fixer.io/latest?base=" + baseCurrency);
+                URLConnection urlConnection = myUrl.openConnection();
+                return ConvertStream(urlConnection.getInputStream());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return new HashMap<String, Double>();
+        }
+
+        @Override
+        protected void onPostExecute(HashMap<String, Double> result) {
+            UsdToCadBaseRate = result.get("CAD");
+        }
+
+        protected HashMap<String, Double> ConvertStream(InputStream is) throws IOException {
+            if (is != null) {
+                JsonReader reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+                try {
+                    return ReadJson(reader);
+                } finally {
+                    reader.close();
+                }
+            } else {
+                return new HashMap<String, Double>();
+            }
+        }
+
+        public HashMap<String, Double> ReadJson(JsonReader reader) throws IOException {
+            HashMap<String, Double> dictionary = new HashMap<String, Double>();
+
+            reader.beginObject();
+
+            while (reader.hasNext()) {
+                String name = reader.nextName();
+
+                if (name.equals("rates")) {
+                    reader.beginObject();
+                    while (reader.hasNext()) {
+                        dictionary.put(reader.nextName(), reader.nextDouble());
+                    }
+                    reader.endObject();
+                } else {
+                    reader.skipValue();
+                }
+            }
+            reader.endObject();
+            return dictionary;
         }
     }
 }
